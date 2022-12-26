31ed052b5f1e20f17f32187d143977e2e7299ce5
Yana <nikandrova0915@gmail.com>
2022-12-26 10:13:39 +0300

    Задача3

---
+++ Task3/Program.cs
@@ -0,0 +1,48 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+void AverageColumns(int[,] array)
+{
+    Console.Write("Среднее арифметическое каждого столбца: ");
+    for (int i = 0; i < array.GetLength(1); i++)
+    {
+        double count = 0;
+        for (int j = 0; j < array.GetLength(0); j++)
+        {
+            count = count + array[j, i];
+        }
+        Console.Write(Math.Round(count / array.GetLength(0), 1) + ",   ");
+
+    }
+}
+
+int[,] FillArray(int m, int n)
+{
+    int[,] array = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            array[i, j] = new Random().Next(0, 10);
+            Console.Write($"{array[i, j]}    ");
+        }
+        Console.WriteLine();
+    }
+    return array;
+}
+
+
+int EnterData(string text)
+{
+    Console.WriteLine(text);
+    int num = int.Parse(Console.ReadLine());
+    return num;
+}
+int m = EnterData("Введите число строк: ");
+int n = EnterData("Введите число столбцов: ");
+AverageColumns(FillArray(m, n));
